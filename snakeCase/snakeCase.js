56804401f68dbaba3abef0c205f592eea1ef1849
rdkerr <rogerdkerr@gmail.com>
2018-11-26 15:15:56 -0700

    Finished snake case

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,15 @@
-const snakeCase = function() {
+const snakeCase = function(string) {
+    // wtf
+    string = string.replace(/\.\./g," ");
+    // camel case
+    if (string.indexOf(" ") < 0) {
+        string = string.replace(/([A-Z])/g,' $1');
+    }
+    return string.toLowerCase()
+        .replace(/[,-?]/g," ")
+        .replace(/\s{2,}/g," ").trim().split(" ")
+        .join("_");
 
 }
 
-module.exports = snakeCase
+module.exports = snakeCase;
