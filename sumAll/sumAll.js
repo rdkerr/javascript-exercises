84a8e9417457fb737b284ec4ed8e23dd1f696126
rdkerr <rogerdkerr@gmail.com>
2018-11-26 11:38:48 -0700

    Finished sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(typeof start === "number" && typeof end === "number" &&
+            start >= 0 && end>=0) {
+        if (start > end) {
+            let swap = start;
+            start = end;
+            end = swap;
+        }
+        return [...Array(end-start+1).keys()].map(i=>i+start).reduce((i,e)=>i+e);
+        
+    } else {
+        return "ERROR";
+    }
 }
 
 module.exports = sumAll
