3cf8e387dd43eb051080c9b75effe3049da2279d
rdkerr <rogerdkerr@gmail.com>
2018-11-26 10:08:08 -0700

    Finished repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    if (count < 0) {
+        return 'ERROR';
+    }
+    let result = '';
+    for (let i = 0 ; i < count ; i ++) {
+        result += string;
+    }
+    return result;
 }
 
-module.exports = repeatString
+module.exports = repeatString;
