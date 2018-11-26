f8099826ea3b8f1af74232fd1dd88f165160234c
rdkerr <rogerdkerr@gmail.com>
2018-11-26 12:13:51 -0700

    Finished tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(degrees) {
+  return Math.round(((degrees - 32) * 5 / 9) * 10) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function(degrees) {
+  return Math.round(((degrees * 9 / 5) + 32) * 10) / 10;
 }
 
 module.exports = {
