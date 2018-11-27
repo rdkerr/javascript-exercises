edded8095b7061682de26e296fb54d3e2010d463
rdkerr <rogerdkerr@gmail.com>
2018-11-26 23:59:08 -0700

    Finished caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,22 @@
-const caesar = function() {
-
+const caesar = function(string, shift) {
+    const alphabet = new Array(26).fill(1).map((_, i)=> String.fromCharCode(65+i));
+    return string.split("")
+        .map(e => {
+            const index = alphabet.indexOf(e.toUpperCase());
+            //return index;
+            if (index < 0) {
+                return e;
+            } else if(e===e.toUpperCase()) {
+                return alphabet[mod((index + shift), 26)];
+            } else {
+                return alphabet[mod((index + shift), 26)].toLowerCase();
+            }
+        }).join("");
 }
 
-module.exports = caesar
+var mod = function (n, m) {
+    var remain = n % m;
+    return Math.floor(remain >= 0 ? remain : remain + m);
+};
+
+module.exports = caesar;
