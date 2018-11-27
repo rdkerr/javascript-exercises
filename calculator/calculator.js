9ff050ae0cb01160d3ffff0998e823e4c28e9ef3
rdkerr <rogerdkerr@gmail.com>
2018-11-27 00:13:05 -0700

    Finished calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,31 @@
-function add () {
-	
+function add (num1, num2) {
+	return num1 + num2;
 }
 
-function subtract () {
-	
+function subtract (num1, num2) {
+	return num1 - num2;
 }
 
-function sum () {
-	
+function sum (array) {
+	return array.reduce((a,b)=>a+b,0);
 }
 
-function multiply () {
-	
+function multiply (array) {
+	return array.reduce((a,b)=>a*b,1);
 }
 
-function power() {
-	
+function power(num1, num2) {
+	return num1 ** num2;
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if (num ===0) {
+		return 1;
+	} else if (num < 3) {
+		return num;
+	}
+	let array=new Array(num).fill(1).map((_,i)=>i+1);
+	return array.reduce((a,b)=>a*b,1);
 }
 
 module.exports = {
