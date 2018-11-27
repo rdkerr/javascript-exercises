2411108a7260c0b18f6d208f17332e8e44c55c74
rdkerr <rogerdkerr@gmail.com>
2018-11-26 18:48:16 -0700

    Finished palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    let begin = 0;
+    string  = string.replace(/[.,!\s]/g, "").toLowerCase();
+    let end = string.length-1;
+    while (begin < end) {
+        if(string[begin]!==string[end]) {
+            return false;
+        }
+        begin += 1;
+        end -= 1;
+    }
+    return true;
 }
 
 module.exports = palindromes
