96bd55604871bb51fdab07001387cccfbf657a33
rdkerr <rogerdkerr@gmail.com>
2018-11-26 18:28:14 -0700

    Finished pig_latin

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,7 +1,21 @@
-function translate() {
+function translate(string) {
 	// body...
+	return string.split(" ")
+		.map(word => {
+			let index = findVowel(word);
+			let firstHalf = word.slice(0,index);
+			let secondHalf = word.slice(index);
+			return secondHalf + firstHalf + "ay";})
+		.join(" ");
 }
 
+function findVowel(word) {
+	let vowels = word.match(/[aeiou]/g);
+	if (vowels[0]==='u' && word.indexOf('u') === word.indexOf('q')+1) {
+		return word.indexOf(vowels[1]);
+	}
+	return word.indexOf(vowels[0]);
+}
 
 module.exports = {
 	translate
