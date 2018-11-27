665eb2973b07b98d583b10b555f89506ccbc7fa4
rdkerr <rogerdkerr@gmail.com>
2018-11-27 00:17:36 -0700

    Finished fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    } if (typeof num === "string") {
+        num = Number(num);
+    } if (num < 3) {
+        return 1;
+    }
+    let a = 1;
+    let b = 1;
+    for (let i = 3 ; i <= num ; i ++) {
+        let c = b;
+        b = b + a;
+        a = c;
+    }
+    return b;
 }
 
 module.exports = fibonacci
